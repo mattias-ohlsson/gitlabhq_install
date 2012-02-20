4bcbf0d249fa66df64d70b21722e0d14e6ccf038
dearaujoj <jdearaujo@auto-planning.fr>
2012-02-20 20:34:41 +0100

    create default yml files to script

--- ubuntu_gitlab.sh
+++ ubuntu_gitlab.sh
@@ -4,5 +4,12 @@ sudo gem install bundler
 git clone git://github.com/gitlabhq/gitlabhq.git
 cd gitlabhq
 bundle install --without development test
+
+#Create new database.yml file if it doesn't exist yet
+cp -i config/database.yml.example config/database.yml
+
+#Create new gitlab.yml file if it doesn't exist yet
+cp -i config/gitlab.yml.example config/gitlab.yml
+
 bundle exec rake db:setup RAILS_ENV=production
 bundle exec rake db:seed_fu RAILS_ENV=production
